3cf73c6d32be0ad0276caf4871015e6ff98d1e03
Митя <mitya@Mitas-MacBook-Pro.local>
2021-02-23 18:03:15 +0300

    Implemented Store with redux-thunk

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import './App.css'
+
+import { Connected } from 'components/Connected';
+import { reducer } from 'store/reducers';
+//import store from './store';
 
 function App() {
+  const store = createStore(reducer, applyMiddleware(thunk, logger))
+
   return (
-    <div className="App">
-      app
-    </div>
+    <Provider store={store}>
+        <Connected />
+    </Provider>
   );
 }
 
-export default App;
+export default App
